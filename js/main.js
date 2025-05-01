239299d3445bdfaed1bca5e3537b619f42b73d27
pippinbarr <pippin.barr@gmail.com>
2025-05-01 11:52:55 -0400

    Oh floating point numbers.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 Moderator
 Pippin Barr (with text by Claudia Molinari)
 
-Based on something I saw Aymeric Mansoux do for a panel. A way to bring up prompts for discussion without a moderator. In this case it's items from Claudia Molinari's Mindful Game Design Manifesto.
+Based on something I saw Aymeric Mansoux do for a panel. A way to bring up items for discussion without a moderator. In this case it's items from Claudia Molinari's Mindful Game Design Manifesto.
 
 In essence it does a "wheel of fortune" spin through items from the manifesto, eventually landing on one for the panel to talk through.
 */
@@ -11,45 +11,46 @@ In essence it does a "wheel of fortune" spin through items from the manifesto, e
 
 // The manifesto we're spinning through
 const manifesto = [
-    "1. your game is born the moment you feel excited about the thought of it",
-    "2. your game is a game if you say so",
-    "3. make a game that embraces all that is you",
-    "4. make a game to inspire others to be better",
-    "5. make a game to build values you can be proud of",
-    "6. make a game for you first, then for the others",
-    "7. respect and admire everyone in the team",
-    "8. the things that matter the most are the ones you learn from the design and development process",
-    "9. respect and admire the design and development process",
-    "10. design the conditions to enjoy the process",
-    "11. design the conditions to love your game",
-    "12. your game process is never late, unless you make it so",
-    "13. change the conditions that stop excitement to flow",
-    "14. give your game a purpose",
-    "15. design your game with a vision",
-    "16. allow good intentions, allow culture in",
-    "17. allow good intentions, allow art in",
-    "18. allow good intentions, allow knowledge in",
-    "19. take care of yourself when making a game",
-    "20. take care of others when making a game",
-    "21. do not ruin your daily result by mourning about what must be done tomorrow",
-    "22. it is you who creates what you feel about your game, be solid",
-    "23. it is your game that creates what you feel about yourself, be fluid",
-    "24. accept the feelings of hate, failure, anxiety and despair, then move on",
-    "25. when lost, think about the step not the stair in front of you",
-    "26. when lost, read or write a poem to your game, it will thank you later",
-    "27. don’t believe everything you think about your game, believe everything you feel",
-    "28. great game ideas can die, let them go",
-    "29. stay present, your game is not what it was or what it will be, it is what it is now",
-    "30. every game idea will end in different ways and will have its own life cycle, respect it",
-    "31. respect the shape your game takes",
-    "32. make a game for its process not for its outcome",
-    "33. you might not always know where your game is heading to, but your game does",
-    "34. appreciate and be grateful about the result of your game",
-    "35. your game is beautiful",
+    "your game is born the moment you feel excited about the thought of it",
+    "your game is a game if you say so",
+    "make a game that embraces all that is you",
+    "make a game to inspire others to be better",
+    "make a game to build values you can be proud of",
+    "make a game for you first, then for the others",
+    "respect and admire everyone in the team",
+    "the things that matter the most are the ones you learn from the design and development process",
+    "respect and admire the design and development process",
+    "design the conditions to enjoy the process",
+    "design the conditions to love your game",
+    "your game process is never late, unless you make it so",
+    "change the conditions that stop excitement to flow",
+    "give your game a purpose",
+    "design your game with a vision",
+    "allow good intentions, allow culture in",
+    "allow good intentions, allow art in",
+    "allow good intentions, allow knowledge in",
+    "take care of yourself when making a game",
+    "take care of others when making a game",
+    "do not ruin your daily result by mourning about what must be done tomorrow",
+    "it is you who creates what you feel about your game, be solid",
+    "it is your game that creates what you feel about yourself, be fluid",
+    "accept the feelings of hate, failure, anxiety and despair, then move on",
+    "when lost, think about the step not the stair in front of you",
+    "when lost, read or write a poem to your game, it will thank you later",
+    "don’t believe everything you think about your game, believe everything you feel",
+    "great game ideas can die, let them go",
+    "stay present, your game is not what it was or what it will be, it is what it is now",
+    "every game idea will end in different ways and will have its own life cycle, respect it",
+    "respect the shape your game takes",
+    "make a game for its process not for its outcome",
+    "you might not always know where your game is heading to, but your game does",
+    "appreciate and be grateful about the result of your game",
+    "your game is beautiful",
 ];
 
-// The prompt element on the page
-const prompt = document.getElementById("prompt");
+// The item element on the page
+const number = document.getElementById("number");
+const item = document.getElementById("item");
 // Our sound effect for the "wheel"
 const tick = new Audio("assets/sounds/click.wav")
 // It spins on a click
@@ -59,7 +60,7 @@ document.addEventListener("click", spinThatWheel);
  * Start spinning the wheel
  */
 function spinThatWheel() {
-    spin(99 + Math.random() * 2, 1, undefined);
+    spin(98 + Math.floor(Math.random() * 4), 1, undefined);
 }
 
 /**
@@ -68,6 +69,8 @@ function spinThatWheel() {
  * show the same thing twice;
  */
 function spin(count, delay, previous) {
+    console.log(count);
+
     // Choose the next item to show
     let next;
     do {
@@ -75,7 +78,8 @@ function spin(count, delay, previous) {
     } while (next === previous);
 
     // Show it
-    prompt.innerText = next;
+    number.innerText = manifesto.indexOf(next) + 1 + ". ";
+    item.innerText = next;
     // Remember it for the next spin to avoid repetition
     previous = next;
 
@@ -101,7 +105,7 @@ function spin(count, delay, previous) {
         delay *= 1.065 + Math.random() * 0.001;
     }
     // Decrease the count, since we just showed one
-    count--;
+    count -= 1;
 
     // Schedule the next ticking into place
     setTimeout(() => {
